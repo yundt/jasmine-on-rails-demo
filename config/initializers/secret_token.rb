4a0c6017a11b5a75638201cc924e75693f9e2941
Marek Nowak <mareknowakk@gmail.com>
2012-07-17 09:57:12 +0200

    inits a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasmineRailsDemo::Application.config.secret_token = 'b3385fc944c6ebae52f834a8b4a3d43bca5fc8b335e82e0ab98e7d4a0960418e18dc5bad2f715817877f6b29e65b55fdddd7c107ee05cba2b3d9a1b019459919'
